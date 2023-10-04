3ad149b5ae4adcc99632de5c507b1e4b78f991c2
trany3004 <trany3004@gmail.com>
2023-10-05 01:21:58 +0700

    init

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost: "http://udagram-yttn2.eba-zjpepdda.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
